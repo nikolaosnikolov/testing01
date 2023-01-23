e6dbdb3315526d2fbee84681542b03958d52739e
Nikos Nikolov <103443152+nikolaosnikolov@users.noreply.github.com>
2023-01-23 12:22:55 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Howdy Hey Everyone
+    Learn React
         </a>
       </header>
     </div>
